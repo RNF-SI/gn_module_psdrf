b114da1ccec0cb7f7206bd404b5ee4ef6f21510e
Augustin Roche <augustin.roche-rnf@espaces-naturels.fr>
2019-04-09 09:44:46 +0200

    routes fonctionnelles

--- backend/blueprint.py
+++ backend/blueprint.py
@@ -2,16 +2,31 @@ from flask import Blueprint
 
 from geonature.utils.env import DB
 from geonature.utils.utilssqlalchemy import json_resp
-from .models import TPrograms
+from .models import TDispositifs, TPlacettes, TArbres
 
-blueprint = Blueprint('cmr', __name__)
+blueprint = Blueprint('psdrf', __name__)
 
 @blueprint.route('/test', methods=['GET', 'POST'])
 def test():
-    return 'It works'
+    return 'It works (ça marche !)'
 
-@blueprint.route('/programs', methods=['GET'])
+@blueprint.route('/dispositifs', methods=['GET'])
 @json_resp
-def get_programs():
-    pgs = DB.session.query(TPrograms).all()
+def get_disps():
+    pgs = DB.session.query(TDispositifs).all()
+    return [pg.as_dict() for pg in pgs]
+
+
+@blueprint.route('/placettes/<int:id_dispositif>', methods=['GET'])
+@json_resp
+def get_placettes(id_dispositif):
+    pgs = DB.session.query(TPlacettes).filter(TPlacettes.id_dispositif == id_dispositif).all()
+    return [pg.as_geofeature('geom', 'id_placette') for pg in pgs]
+
+
+@blueprint.route('/arbres/<int:id_dispositif>', methods=['GET'])
+@json_resp
+def get_arbres(id_dispositif):
+    """ Recherche tous les arbres d'un dispositif donné """
+    pgs = DB.session.query(TArbres).filter(TArbres.placette.id_dispositif == id_dispositif).all()
     return [pg.as_dict() for pg in pgs]
--- frontend/app/module.config.ts
+++ frontend/app/module.config.ts
@@ -1,1 +1,5 @@
-export const ModuleConfig = {}
+export const ModuleConfig = {
+ "ID_MODULE": 9,
+ "MODULE_CODE": "PSDRF",
+ "MODULE_URL": "psdrf"
+}
