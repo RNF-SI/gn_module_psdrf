6494a02495c47929bfd3210afa2caaa58ae8ec11
aschlegle <antoine.schlegel01@gmail.com>
2024-02-22 10:07:07 +0100

    add exception on editCarnet celery task

--- backend/gn_module_psdrf/tasks.py
+++ backend/gn_module_psdrf/tasks.py
@@ -16,41 +16,40 @@ logger = get_task_logger(__name__)
 @celery_app.task(bind=True, soft_time_limit=700, time_limit=900)
 def test_celery(self, id_dispositif, isCarnetToDownload, isPlanDesArbresToDownload, carnetToDownloadParameters, outFilePath):
     logger.info(f"Starting carnet d'analyse of dispositif {id_dispositif}.")
+    temp_file = None  # Initialize temp_file and zipName before try-except block
+    zipName = f'documents_dispositif-{str(id_dispositif)}.zip'
     try:
         data_analysis(str(id_dispositif), isCarnetToDownload, isPlanDesArbresToDownload, carnetToDownloadParameters)
-    except SoftTimeLimitExceeded:
-        # Handle the soft time limit, e.g., cleanup, logging, etc.
-        pass
+        
+        base_dir = os.path.dirname(os.path.abspath(__file__))  # Gets the directory of the current script
+        output_dir = os.path.join(base_dir, 'Rscripts/zip')
+        
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        temp_file = tempfile.NamedTemporaryFile(dir=output_dir, delete=False, suffix=".zip")
+        outFilePath = os.path.join(base_dir, 'Rscripts/out')
+        
+        with zipfile.ZipFile(temp_file.name, 'w', zipfile.ZIP_DEFLATED) as zf:
+            for dirname, subdirs, files in os.walk(outFilePath):
+                for filename in files:
+                    absname = os.path.abspath(os.path.join(dirname, filename))
+                    arcname = absname[len(outFilePath) + 1:]
+                    if (arcname != '.gitignore') and (not arcname.endswith(('.log', '.tex'))):
+                        zf.write(absname, arcname)
+
+        os.chmod(temp_file.name, 0o777)
+        
+        self.update_state(state='SUCCESS', meta={'file_path': temp_file.name, "file_name": zipName})
+
+    except SoftTimeLimitExceeded as e:
+        logger.exception("Soft time limit exceed for task id %s", self.request.id)
+        self.update_state(state='FAILURE', meta={'exc_type': str(type(e).__name__), 'exc_message': str(e)})
+        raise e
     except Exception as e:
-        logging.critical(e)
-        msg = json.dumps({"type": "bug", "msg": "Unkown error during analysis"})
-        logging.info(msg)
-        return Response(msg, status=500)
+        logger.exception("Error during processing task id %s", self.request.id)
+        logger.exception("Error message %s", str(e))
+        self.update_state(state='FAILURE', meta={'exc_type': str(type(e).__name__), 'exc_message': str(e)})
+        raise e
 
-    base_dir = os.path.dirname(os.path.abspath(__file__))  # Gets the directory of the current script
-    output_dir = os.path.join(base_dir, 'Rscripts/zip')
-
-    # Ensure the directory exists
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    # Now, you can create the temporary file
-    temp_file = tempfile.NamedTemporaryFile(dir=output_dir, delete=False, suffix=".zip")
-
-    # The directory you want to archive
-    outFilePath = os.path.join(base_dir, 'Rscripts/out')
-
-    with zipfile.ZipFile(temp_file.name, 'w', zipfile.ZIP_DEFLATED) as zf:
-        for dirname, subdirs, files in os.walk(outFilePath):
-            for filename in files:
-                absname = os.path.abspath(os.path.join(dirname, filename))
-                arcname = absname[len(outFilePath) + 1:]  # +1 to remove the leading slash or backslash
-                if (arcname != '.gitignore') and (not arcname.endswith(('.log', '.tex'))):
-                    zf.write(absname, arcname)
-
-    os.chmod(temp_file.name, 0o777)
-
-    zipName = 'documents_dispositif-'+str(id_dispositif)+'.zip'
-
-    return {"file_path": temp_file.name, "file_name": zipName}
-
+    return {"file_path": temp_file.name if temp_file else "N/A", "file_name": zipName}
