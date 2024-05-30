174a880deb746d6a79b08ffe3b3b108bc26bafa7
aschlegle <antoine.schlegel01@gmail.com>
2024-05-30 13:59:05 +0200

    download dendro3 with loading

--- frontend/app/components/dendro-download.component.ts
+++ frontend/app/components/dendro-download.component.ts
@@ -7,6 +7,8 @@ import { PsdrfDataService } from "../services/route.service";
   styleUrls: ["dendro-download.component.scss"]
 })
 export class DendroDownloadComponent implements OnInit {
+  loading = false;  // Add loading state
+
   constructor(
     private dataSrv: PsdrfDataService, 
 
@@ -15,17 +17,23 @@ export class DendroDownloadComponent implements OnInit {
   ngOnInit() {}
 
   downloadDendro() {
+    this.loading = true;  // Set loading to true when download starts
     this.dataSrv.getDendroApk().subscribe(
       blob => {
         const url = window.URL.createObjectURL(blob);
         const anchor = document.createElement('a');
-        anchor.download = 'Dendro3.apk';
+        anchor.style.display = 'none';
+        document.body.appendChild(anchor);
         anchor.href = url;
+        anchor.download = 'Dendro3.apk';
         anchor.click();
         window.URL.revokeObjectURL(url);  // Clean up the URL object
+        document.body.removeChild(anchor);
+        this.loading = false;  // Set loading to false when download completes
       },
       error => {
         console.error('Erreur de Téléchargement:', error);
+        this.loading = false;  // Set loading to false if an error occurs
       }
     );
   }
