5c8930456819c015c5be53887f94e7a7f4238695
aschlegle <antoine.schlegel01@gmail.com>
2024-06-05 12:18:02 +0200

    Use corrent_app context in celery tasks

--- backend/gn_module_psdrf/tasks.py
+++ backend/gn_module_psdrf/tasks.py
@@ -1,4 +1,4 @@
-from flask import Response
+from flask import Response, current_app
 from geonature.utils.celery import celery_app
 import logging
 import json
@@ -37,26 +37,27 @@ def test_celery(self, id_dispositif, isCarnetToDownload, isPlanDesArbresToDownlo
     temp_file = None  # Initialize temp_file and zipName before try-except block
     zipName = f'documents_dispositif-{str(id_dispositif)}.zip'
     try:
-        data_analysis(str(id_dispositif), isCarnetToDownload, isPlanDesArbresToDownload, carnetToDownloadParameters)
+        with current_app.app_context():
+            data_analysis(str(id_dispositif), isCarnetToDownload, isPlanDesArbresToDownload, carnetToDownloadParameters)
         
-        base_dir = os.path.dirname(os.path.abspath(__file__))  # Gets the directory of the current script
-        output_dir = os.path.join(base_dir, 'Rscripts/zip')
+            base_dir = os.path.dirname(os.path.abspath(__file__))  # Gets the directory of the current script
+            output_dir = os.path.join(base_dir, 'Rscripts/zip')
         
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
 
-        temp_file = tempfile.NamedTemporaryFile(dir=output_dir, delete=False, suffix=".zip")
-        outFilePath = os.path.join(base_dir, 'Rscripts/out')
+            temp_file = tempfile.NamedTemporaryFile(dir=output_dir, delete=False, suffix=".zip")
+            outFilePath = os.path.join(base_dir, 'Rscripts/out')
         
-        with zipfile.ZipFile(temp_file.name, 'w', zipfile.ZIP_DEFLATED) as zf:
-            for dirname, subdirs, files in os.walk(outFilePath):
-                for filename in files:
-                    absname = os.path.abspath(os.path.join(dirname, filename))
-                    arcname = absname[len(outFilePath) + 1:]
-                    if (arcname != '.gitignore') and (not arcname.endswith(('.log', '.tex'))):
-                        zf.write(absname, arcname)
+            with zipfile.ZipFile(temp_file.name, 'w', zipfile.ZIP_DEFLATED) as zf:
+                for dirname, subdirs, files in os.walk(outFilePath):
+                    for filename in files:
+                        absname = os.path.abspath(os.path.join(dirname, filename))
+                        arcname = absname[len(outFilePath) + 1:]
+                        if (arcname != '.gitignore') and (not arcname.endswith(('.log', '.tex'))):
+                            zf.write(absname, arcname)
 
-        os.chmod(temp_file.name, 0o777)
+            os.chmod(temp_file.name, 0o777)
         
         self.update_state(state='SUCCESS', meta={'file_path': temp_file.name, "file_name": zipName})
 
@@ -71,9 +72,6 @@ def test_celery(self, id_dispositif, isCarnetToDownload, isPlanDesArbresToDownlo
         raise e
 
     return {"file_path": temp_file.name if temp_file else "N/A", "file_name": zipName}
-
-
-
 
 @celery_app.task(bind=True, soft_time_limit=2400, time_limit=2600)
 def insert_or_update_data(self, data):
@@ -135,66 +133,66 @@ def insert_or_update_data(self, data):
 
     print("start insert_or_update_data")
     try:
-        if 'id_dispositif' in data:
-            result = insert_or_update_dispositif(data)
-            if 'cycles' in data:
-                for cycle_data in data['cycles']:
-                    cycle_result = insert_or_update_cycle(cycle_data)
+        with current_app.app_context():
+            if 'id_dispositif' in data:
+                result = insert_or_update_dispositif(data)
+                if 'cycles' in data:
+                    for cycle_data in data['cycles']:
+                        cycle_result = insert_or_update_cycle(cycle_data)
 
-            if 'placettes' in data:
-                for placette_data in data['placettes']:
-                    placette_result = insert_or_update_placette(placette_data)
+                if 'placettes' in data:
+                    for placette_data in data['placettes']:
+                        placette_result = insert_or_update_placette(placette_data)
                     
-                    created_arbres_temp, counts_arbre_temp, counts_arbre_mesure_temp = insert_update_or_delete_arbre(placette_data)
-                    created_arbres.extend(created_arbres_temp)
-                    counts_arbre["created"] += counts_arbre_temp["created"]
-                    counts_arbre["updated"] += counts_arbre_temp["updated"]
-                    counts_arbre["deleted"] += counts_arbre_temp["deleted"]
-                    counts_arbre_mesure["created"] += counts_arbre_mesure_temp["created"]
-                    counts_arbre_mesure["updated"] += counts_arbre_mesure_temp["updated"]
-                    counts_arbre_mesure["deleted"] += counts_arbre_mesure_temp["deleted"]
+                        created_arbres_temp, counts_arbre_temp, counts_arbre_mesure_temp = insert_update_or_delete_arbre(placette_data)
+                        created_arbres.extend(created_arbres_temp)
+                        counts_arbre["created"] += counts_arbre_temp["created"]
+                        counts_arbre["updated"] += counts_arbre_temp["updated"]
+                        counts_arbre["deleted"] += counts_arbre_temp["deleted"]
+                        counts_arbre_mesure["created"] += counts_arbre_mesure_temp["created"]
+                        counts_arbre_mesure["updated"] += counts_arbre_mesure_temp["updated"]
+                        counts_arbre_mesure["deleted"] += counts_arbre_mesure_temp["deleted"]
 
-                    created_bms_temp, counts_bm_temp, counts_bm_mesure_temp = insert_update_or_delete_bms(placette_data)
-                    created_bms.extend(created_bms_temp)
-                    counts_bm["created"] += counts_bm_temp["created"]
-                    counts_bm["updated"] += counts_bm_temp["updated"]
-                    counts_bm["deleted"] += counts_bm_temp["deleted"]
-                    counts_bm_mesure["created"] += counts_bm_mesure_temp["created"]
-                    counts_bm_mesure["updated"] += counts_bm_mesure_temp["updated"]
-                    counts_bm_mesure["deleted"] += counts_bm_mesure_temp["deleted"]
+                        created_bms_temp, counts_bm_temp, counts_bm_mesure_temp = insert_update_or_delete_bms(placette_data)
+                        created_bms.extend(created_bms_temp)
+                        counts_bm["created"] += counts_bm_temp["created"]
+                        counts_bm["updated"] += counts_bm_temp["updated"]
+                        counts_bm["deleted"] += counts_bm_temp["deleted"]
+                        counts_bm_mesure["created"] += counts_bm_mesure_temp["created"]
+                        counts_bm_mesure["updated"] += counts_bm_mesure_temp["updated"]
+                        counts_bm_mesure["deleted"] += counts_bm_mesure_temp["deleted"]
 
-                    counts_repere_temp = insert_update_or_delete_repere(placette_data)
-                    counts_repere["created"] += counts_repere_temp["created"]
-                    counts_repere["updated"] += counts_repere_temp["updated"]
-                    counts_repere["deleted"] += counts_repere_temp["deleted"]
-                    logger.info(f"Starting carnet d'analyse of dispositif {counts_repere['created']}.")
+                        counts_repere_temp = insert_update_or_delete_repere(placette_data)
+                        counts_repere["created"] += counts_repere_temp["created"]
+                        counts_repere["updated"] += counts_repere_temp["updated"]
+                        counts_repere["deleted"] += counts_repere_temp["deleted"]
+                        logger.info(f"Starting carnet d'analyse of dispositif {counts_repere['created']}.")
 
+                        counts_cor_cycle_placette_temp, counts_regeneration_temp, counts_transect_temp = insert_or_update_cor_cycle_placette(placette_data)
+                        counts_cor_cycle_placette["created"] += counts_cor_cycle_placette_temp["created"]
+                        counts_cor_cycle_placette["updated"] += counts_cor_cycle_placette_temp["updated"]
+                        counts_cor_cycle_placette["deleted"] += counts_cor_cycle_placette_temp["deleted"]
+                        counts_regeneration["created"] += counts_regeneration_temp["created"]
+                        counts_regeneration["updated"] += counts_regeneration_temp["updated"]
+                        counts_regeneration["deleted"] += counts_regeneration_temp["deleted"]
+                        counts_transect["created"] += counts_transect_temp["created"]
+                        counts_transect["updated"] += counts_transect_temp["updated"]
+                        counts_transect["deleted"] += counts_transect_temp["deleted"]
 
-                    counts_cor_cycle_placette_temp, counts_regeneration_temp, counts_transect_temp = insert_or_update_cor_cycle_placette(placette_data)
-                    counts_cor_cycle_placette["created"] += counts_cor_cycle_placette_temp["created"]
-                    counts_cor_cycle_placette["updated"] += counts_cor_cycle_placette_temp["updated"]
-                    counts_cor_cycle_placette["deleted"] += counts_cor_cycle_placette_temp["deleted"]
-                    counts_regeneration["created"] += counts_regeneration_temp["created"]
-                    counts_regeneration["updated"] += counts_regeneration_temp["updated"]
-                    counts_regeneration["deleted"] += counts_regeneration_temp["deleted"]
-                    counts_transect["created"] += counts_transect_temp["created"]
-                    counts_transect["updated"] += counts_transect_temp["updated"]
-                    counts_transect["deleted"] += counts_transect_temp["deleted"]
-
-        self.update_state(
-            state='SUCCESS', 
-            meta={
-                'created_arbres': created_arbres, 
-                'counts_arbre': counts_arbre, 
-                'counts_arbre_mesure':counts_arbre_mesure, 
-                'created_bms':created_bms, 
-                'counts_bm': counts_bm, 
-                'counts_bm_mesure': counts_bm_mesure, 
-                'counts_repere': counts_repere, 
-                'counts_cor_cycle_placette': counts_cor_cycle_placette, 
-                'counts_regeneration': counts_regeneration, 
-                'counts_transect': counts_transect,
-                })
+            self.update_state(
+                state='SUCCESS', 
+                meta={
+                    'created_arbres': created_arbres, 
+                    'counts_arbre': counts_arbre, 
+                    'counts_arbre_mesure':counts_arbre_mesure, 
+                    'created_bms':created_bms, 
+                    'counts_bm': counts_bm, 
+                    'counts_bm_mesure': counts_bm_mesure, 
+                    'counts_repere': counts_repere, 
+                    'counts_cor_cycle_placette': counts_cor_cycle_placette, 
+                    'counts_regeneration': counts_regeneration, 
+                    'counts_transect': counts_transect,
+                    })
 
     except SoftTimeLimitExceeded as e:
         logger.exception("Soft time limit exceed for task id %s", self.request.id)
@@ -223,15 +221,16 @@ def insert_or_update_data(self, data):
 def fetch_dispositif_data(self, id_dispositif):
     logger.info(f"Task started for dispositif ID: {id_dispositif}")
     try:
-        logger.info("Starting database query...")
-        query = DB.session.query(TDispositifs).filter(TDispositifs.id_dispositif == id_dispositif).one()
-        logger.info("Database query completed.")
-        schema = DispositifSchema(many=False)
-        logger.info("Starting serialization of data...")
-        result = schema.dump(query)
-        logger.info("Serialization completed.")
-        logger.info(f"Successfully fetched and serialized data for dispositif ID: {id_dispositif}")
-        return {'status': 'SUCCESS', 'data': result}
+        with current_app.app_context():
+            logger.info("Starting database query...")
+            query = DB.session.query(TDispositifs).filter(TDispositifs.id_dispositif == id_dispositif).one()
+            logger.info("Database query completed.")
+            schema = DispositifSchema(many=False)
+            logger.info("Starting serialization of data...")
+            result = schema.dump(query)
+            logger.info("Serialization completed.")
+            logger.info(f"Successfully fetched and serialized data for dispositif ID: {id_dispositif}")
+            return {'status': 'SUCCESS', 'data': result}
     except Exception as e:
         logger.exception("Error during fetching dispositif data")
         return {'status': 'FAILURE', 'data': str(e)}
@@ -244,77 +243,77 @@ def fetch_updated_data(self, id_dispositif, last_sync):
     logger.info(f"Fetching updated data for dispositif ID: {id_dispositif} since last sync: {last_sync}")
 
     try:
+        with current_app.app_context():
+            logger.info("Starting database query...")
+            logger.error("Fetching data from staging DB")
+            # Fetch the entire dispositif including all related entities
+            dispositif = (
+                DB.session.query(TDispositifsStaging)
+                .filter(TDispositifsStaging.id_dispositif == id_dispositif)
+                .first()
+            )
 
-        logger.info("Starting database query...")
-        logger.error("Fetching data from staging DB")
-        # Fetch the entire dispositif including all related entities
-        dispositif = (
-            DB.session.query(TDispositifsStaging)
-            .filter(TDispositifsStaging.id_dispositif == id_dispositif)
-            .first()
-        )
+            if not dispositif:
+                # Handle the no data case as a normal situation
+                logger.info(f"No data found for dispositif ID {id_dispositif}. No updates needed.")
+                return {'status': 'SUCCESS', 'data': None, 'message': 'No data available to update'}
 
-        if not dispositif:
-            # Handle the no data case as a normal situation
-            logger.info(f"No data found for dispositif ID {id_dispositif}. No updates needed.")
-            return {'status': 'SUCCESS', 'data': None, 'message': 'No data available to update'}
+            # Using the schema to serialize the data
+            dispositif_schema = DispositifStagingSchema(many=False)
+            full_data = dispositif_schema.dump(dispositif)
 
-        # Using the schema to serialize the data
-        dispositif_schema = DispositifStagingSchema(many=False)
-        full_data = dispositif_schema.dump(dispositif)
+            counts = count_updates_and_creations(full_data, last_sync_date)
 
-        counts = count_updates_and_creations(full_data, last_sync_date)
+            # Filtering the serialized data
+            filtered_data = {
+                'cycles': [
+                    {
+                        **cycle,
+                        'corCyclesPlacettes': [
+                            {
+                                **cor_cycle,
+                                'regenerations': [
+                                    reg for reg in cor_cycle['regenerations'] if reg['updated_at'] > last_sync_date
+                                ],
+                                'transects': [
+                                    tran for tran in cor_cycle['transects'] if tran['updated_at'] > last_sync_date
+                                ]
+                            } 
+                            for cor_cycle in cycle['corCyclesPlacettes'] if cor_cycle['updated_at'] > last_sync_date
+                        ]
+                    }
+                    for cycle in full_data['cycles']
+                ],
+                'placettes': [
+                    {
+                        **placette,
+                        'arbres': [
+                            {
+                                **arbre,
+                                'arbres_mesures': [
+                                    mesure for mesure in arbre['arbres_mesures'] if mesure['updated_at'] > last_sync_date
+                                ]
+                            }
+                            for arbre in placette['arbres'] if arbre['updated_at'] > last_sync_date
+                        ],
+                        'bmsSup30': [
+                            {
+                                **bms,
+                                'bm_sup_30_mesures': [
+                                    mesure for mesure in bms['bm_sup_30_mesures'] if mesure['updated_at'] > last_sync_date
+                                ]
+                            }
+                            for bms in placette['bmsSup30'] if bms['updated_at'] > last_sync_date
+                        ],
+                        'reperes': [
+                            rep for rep in placette['reperes'] if rep['updated_at'] > last_sync_date
+                        ]
+                    }
+                    for placette in full_data['placettes']
+                ]
+            }
 
-        # Filtering the serialized data
-        filtered_data = {
-            'cycles': [
-                {
-                    **cycle,
-                    'corCyclesPlacettes': [
-                        {
-                            **cor_cycle,
-                            'regenerations': [
-                                reg for reg in cor_cycle['regenerations'] if reg['updated_at'] > last_sync_date
-                            ],
-                            'transects': [
-                                tran for tran in cor_cycle['transects'] if tran['updated_at'] > last_sync_date
-                            ]
-                        } 
-                        for cor_cycle in cycle['corCyclesPlacettes'] if cor_cycle['updated_at'] > last_sync_date
-                    ]
-                }
-                for cycle in full_data['cycles']
-            ],
-            'placettes': [
-                {
-                    **placette,
-                    'arbres': [
-                        {
-                            **arbre,
-                            'arbres_mesures': [
-                                mesure for mesure in arbre['arbres_mesures'] if mesure['updated_at'] > last_sync_date
-                            ]
-                        }
-                        for arbre in placette['arbres'] if arbre['updated_at'] > last_sync_date
-                    ],
-                    'bmsSup30': [
-                        {
-                            **bms,
-                            'bm_sup_30_mesures': [
-                                mesure for mesure in bms['bm_sup_30_mesures'] if mesure['updated_at'] > last_sync_date
-                            ]
-                        }
-                        for bms in placette['bmsSup30'] if bms['updated_at'] > last_sync_date
-                    ],
-                    'reperes': [
-                        rep for rep in placette['reperes'] if rep['updated_at'] > last_sync_date
-                    ]
-                }
-                for placette in full_data['placettes']
-            ]
-        }
-
-        return {'status': 'SUCCESS', 'data': filtered_data, 'counts': counts}
+            return {'status': 'SUCCESS', 'data': filtered_data, 'counts': counts}
     except Exception as e:
         logger.exception("Failed to fetch or serialize updated data", exc_info=e)
         return {'status': 'FAILURE', 'data': str(e)}
